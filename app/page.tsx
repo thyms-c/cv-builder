9a1793b74e79912a295e8ca5a9b7a3d0528cad59
thyms-c <thanyamas.c@ku.th>
2024-03-12 23:08:18 +0700

    fix home

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import { getSession } from "@/lib/auth"
-import { redirect } from "next/navigation"
-import Home from "../components/home"
+import { getSession } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import Home from "../components/home";
 
 export default async function Page() {
-  const session = await getSession()
+  const session = await getSession();
 
-  if (session) {
-    redirect("/workspace")
-  }
+  // if (session) {
+  //   redirect("/workspace")
+  // }
   return (
     <>
       <Home />
     </>
-  )
+  );
 }
